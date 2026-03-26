b6ddbc61b72c298d967c965503b5c16221e79965
Lorenzo <lorenzodavanco@hotmail.com>
2026-03-26 17:01:47 -0300

    quase

--- ArvoreGen.cpp
+++ ArvoreGen.cpp
@@ -27,18 +27,13 @@ PONT criaNo() {
     cin.ignore();
     cout << "nascimento: " << endl;
     cin >> novo->nascimento;
+    cin.ignore();
     return novo;
 }
 
 PONT inicializar() {
     return criaNo();
 }
-
-PONT buscaAux(PONT raiz, string nome) { 
-    if (raiz == NULL) return NULL;
-    if (raiz->nome == nome) return raiz;
-    return buscaAux(raiz->irmaos, nome);
-    }
 
 PONT buscarFam(PONT raiz, string nome) {
     if (raiz == NULL) return NULL;
@@ -89,17 +84,25 @@ bool inserir(PONT raiz, string origin, string nome, parentesco pa) { //
             origem->mae = novo;
             return true;
     }
-    novo = inserirAux(raiz,origem->irmaos,nome, pa);
-    origem->irmaos = novo;
+    novo = inserirAux(raiz,origem,nome, pa); // eh irmao
+    PONT next = origem->irmaos;
+    if (next == NULL) {
+        origem->irmaos = novo;
+        return true;
+        }
+    while (next->irmaos) next = next->irmaos;
+    next->irmaos = novo;
     return true;
 }
 
 void exibirAux(PONT raiz) {
+    cout << endl;
     cout << "Nome: " << raiz->nome << endl;
     cout << "Sobrenome: " << raiz->sobrenome << endl;
     cout << "Data de nascimento:" << raiz->nascimento << endl;
-    cout << "Pai: " << raiz->pai->nome << endl;
-    cout << "Mae: " << raiz->mae->nome << endl;
+    if (raiz->pai) cout << "Pai: " << raiz->pai->nome << endl;
+    if (raiz->mae) cout << "Mae: " << raiz->mae->nome << endl;
+    cout << endl;
 }
 
 void exibir(PONT raiz) {
@@ -108,14 +111,32 @@ void exibir(PONT raiz) {
     PONT next = raiz->pai;
     while (next) {
         exibir(next);
+        next = next->irmaos;
     }
-    
-
+    next = raiz->mae;
+    while (next) {
+        exibir(next);
+        next = next->irmaos;
+    }
+    next = raiz->irmaos;
+    while (next) {
+        exibir(next);
+        next = next->irmaos;
+    }
 }
 
-
-
 int main() {
-    string c = "po";
-    cout << c << endl;
+    PONT eu;
+    eu = inicializar();
+    exibir(eu);
+    inserir(eu,"lorenzo","alexandre",pai);
+    inserir(eu,"lorenzo","giovana",mae);
+    inserir(eu,"lorenzo","isabela",irmao);
+    inserir(eu,"giovana","oneide",mae); 
+    inserir(eu,"isabela","aline",irmao);
+    inserir(eu,"aline","ezequiel",pai); 
+    inserir(eu,"isabela","giovana",mae); // parou aqui
+    exibir(eu);
+    exibir(eu);
+
 };
